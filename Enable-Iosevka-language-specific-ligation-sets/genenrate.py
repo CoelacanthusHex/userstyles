17d6c3757fcdc0961b1919a17d5cc56625b5ecdb
Coelacanthus <uwu@coelacanthus.name>
2025-10-21 02:12:44 +0800

    updlet: Enable Iosevka language-specific ligation sets (1.2)
    * Add alias kt/kts for Kotlin.
    * Fix Coq new name (rcoq -> rocq).
    * Add support for Sphinx and Discourses.
    * Add support for Raku official site.
    * Try to add support for MDN, but need to solve CSP and ShadowDOM issue.
    
    Signed-off-by: Coelacanthus <uwu@coelacanthus.name>

--- Enable-Iosevka-language-specific-ligation-sets/genenrate.py
+++ Enable-Iosevka-language-specific-ligation-sets/genenrate.py
@@ -4,11 +4,11 @@
 # SPDX-License-Identifier: MPL-2.0
 
 import itertools
-from typing import Dict, List, TextIO, Iterable
+from typing import Dict, List, TextIO, Iterable, Optional
 
 header: str = """/* ==UserStyle==
 @name           Enable Iosevka language-specific ligation sets
-@version        1.1.0
+@version        1.2.0
 @description    Enable Iosevka language-specific ligation sets for code elements.
 @namespace      Coelacanthus
 @homepageURL    https://github.com/CoelacanthusHex/userstyles
@@ -25,7 +25,9 @@ SPDX-License-Identifier: MPL-2.0
 languages: Dict[str, List[str]] = {
     # FIXME: Don't include Golang because it uses :=.
     "CLIK": [
+        # C
         "c",
+        # C++
         "cpp",
         "cc",
         "c++",
@@ -34,107 +36,221 @@ languages: Dict[str, List[str]] = {
         "hh",
         "hxx",
         "cxx",
+        # Objective-C/Objecive-C++
         "objc",
         "objcpp",
         "objc++",
+        # D
         "d",
+        # Java
         "java",
+        # Kotlin
         "kotlin",
+        "kt",
+        "kts",
+        # C#
         "csharp",
         "cs",
         "c#",
+        # Zig
         "zig",
     ],
     "JSPT": [
+        # JavaScript
         "javascript",
         "js",
         "jsx",
         "mjs",
         "cjs",
+        # TypeScript
         "typescript",
         "ts",
         "tsx",
         "mts",
         "cts",
     ],
-    "PHPX": ["php"],
-    "JLIA": ["julia"],
+    "PHPX": [
+        "php",
+    ],
+    "JLIA": [
+        "julia",
+    ],
     # FIXME: Don't include Perl because it uses <>.
-    "RAKU": ["raku"],
-    "MLXX": ["sml", "smlnj", "ml", "ocaml"],
-    # FIXME: Is "fs" distinguishable between F# and F*?
-    "FSHP": ["fsharp", "fs", "f#"],
-    "FSTA": ["fstar"],
+    "RAKU": [
+        "raku",
+    ],
+    "MLXX": [
+        # ML & SML
+        "sml",
+        "smlnj",
+        "ml",
+        # OCaml
+        "ocaml",
+    ],
+    "FSHP": [
+        "fsharp",
+        # FIXME: Is "fs" distinguishable between F# and F*?
+        "fs",
+        "f#",
+    ],
+    "FSTA": [
+        "fstar",
+    ],
     # FIXME: Don't include Agda because it uses Unicode.
-    "HSKL": ["haskell", "hs"],
-    "IDRS": ["idris", "idr"],
-    "ELMX": ["elm"],
-    "PURS": ["purescript", "purs"],
-    "SWFT": ["swift"],
-    "DFNY": ["dafny"],
-    "COQX": ["coq", "rcoq"],
-    "MTLB": ["matlab"],
-    "WFLM": ["mathematica", "mma", "wl"],
-    "VRLG": ["verilog", "v", "sv", "svh"],
+    "HSKL": ["haskell", "hs", "lhs"],
+    "IDRS": [
+        "idris",
+        "idr",
+    ],
+    "ELMX": [
+        "elm",
+    ],
+    "PURS": [
+        "purescript",
+        "purs",
+    ],
+    "SWFT": [
+        "swift",
+    ],
+    "DFNY": [
+        "dafny",
+    ],
+    "COQX": [
+        "coq",
+        "rocq",
+    ],
+    "MTLB": [
+        "matlab",
+    ],
+    "WFLM": [
+        # Mathematica
+        "mathematica",
+        "mma",
+        # Wolfram Language
+        "wl",
+    ],
+    "VRLG": [
+        # Verilog
+        "verilog",
+        "v",
+        # SystemVerilog
+        "sv",
+        "svh",
+    ],
     # FIXME: default seems best for Scala.
 }
 
 class_keywords_before: List[str] = [
+    # Meow Meow (@CircuitCoder) homebrew highlighter
+    # https://github.com/CircuitCoder/layered/blob/b76a5be5377703e3e781542eee743fca42433c57/gen/src/post/md.rs#L106
     "highlighted",
+    # Very common. Like Shiki, Prism.js, Highlight.js.
+    # https://github.com/PrismJS/prism/blob/9579fc1c2a501506f6a5ee1d007585a08569a067/src/shared/dom-util.js#L30
     "language",
+    # MediaWiki
     "mw-highlight-lang",
+    # Speed-highlight JS
     "shj-lang",
+    # GitHub Markdown
     "highlight-source",
+    # Highlight.js, Discourses
+    "lang",
+    # Sphinx
+    "highlight",
+    # Highlight.js
+    # https://github.com/highlightjs/highlight.js/blob/5697ae5187746c24732e62cd625f3f83004a44ce/src/lib/html_renderer.js#L32
+    "hljs",
+    "hljs-language",
 ]
-class_keywords_after: List[str] = ["file-line"]
-attribute_keywords: List[str] = ["data-langdata-language"]
-combine_classes: List[str] = ["hljs", "highlight"]
+class_keywords_after: List[str] = [
+    "file-line",
+    # https://raku.org/
+    "code",
+]
+attribute_keywords: List[str] = [
+    "data-lang",
+    "data-language",
+    # MDN
+    "language",
+]
+combine_classes: List[str] = [
+    # highlight.js
+    # https://github.com/highlightjs/highlight.js/blob/5697ae5187746c24732e62cd625f3f83004a44ce/src/highlight.js#L731
+    "hljs",
+    # Very Common.
+    "highlight",
+    # MDN
+    # FIXME: It's in ShadowDOM so SHALL be used with inject userscript.
+    #        https://github.com/openstyles/stylus/issues/739
+    #        https://greasyfork.org/en/scripts/424030-stylus-shadow-dom-support
+    #        But all available userscripts failed on Firefox.
+    "brush:",
+]
 
 
-def write_rule(file: TextIO, selectors: Iterable[str], tag: str) -> None:
+def write_rule_for_code_element(
+    file: TextIO, selectors: Iterable[str], tag: str, comment: Optional[str] = None
+) -> None:
     selector = ", ".join(selectors)
-    file.write(f"    :is({selector}) :is(pre, code, textarea),\n")
-    file.write(f"    :is(pre, code):is({selector}) {{\n")
-    file.write(f'        font-feature-settings: "calt" off, "{tag}" on;\n')
-    file.write("    }\n")
+    selectors = [
+        f":is({selector}) :is(pre, code, textarea)",
+        f":is(pre, code):is({selector})",
+    ]
+    selectors_with_shadowdom = itertools.chain.from_iterable(
+        map(lambda x: [f"{x}", f":host {x}"], selectors)
+    )
+    write_rule_with_whole_selectors(file, selectors_with_shadowdom, tag, comment)
+
+
+def write_rule_with_whole_selectors(
+    file: TextIO, selectors: Iterable[str], tag: str, comment: Optional[str] = None
+) -> None:
+    if comment:
+        file.write(f"    /* {comment} */\n")
+    for selector in selectors:
+        file.write(f"    {selector} {{\n")
+        file.write(f'        font-feature-settings: "calt" off, "{tag}" on;\n')
+        file.write("    }\n")
 
 
 def main():
     with open("Enable-Iosevka-language-specific-ligation-sets.user.css", "w") as file:
         file.write(header)
         for tag, lang in languages.items():
-            selectors_class_keyword_before = map(
+            selectors_class_keyword_before: Iterable[str] = map(
                 lambda x: f'[class~="{x}" i]',
                 map(
                     lambda x: "-".join(x),
                     itertools.product(class_keywords_before, lang),
                 ),
             )
-            selectors_class_keyword_after = map(
+            selectors_class_keyword_after: Iterable[str] = map(
                 lambda x: f'[class~="{x}" i]',
                 map(
                     lambda x: "-".join(x), itertools.product(lang, class_keywords_after)
                 ),
             )
-            selectors_attribute_keyword = map(
+            selectors_attribute_keyword: Iterable[str] = map(
                 lambda x: f'[{x[0]}~="{x[1]}" i]',
                 itertools.product(attribute_keywords, lang),
             )
-            selectors_combine_class = map(
+            selectors_combine_class: Iterable[str] = map(
                 lambda x: f'[class~="{x[0]}" i][class~="{x[1]}" i]',
                 itertools.product(combine_classes, lang),
             )
-            write_rule(file, selectors_class_keyword_before, tag)
-            write_rule(file, selectors_class_keyword_after, tag)
-            write_rule(file, selectors_attribute_keyword, tag)
-            write_rule(file, selectors_combine_class, tag)
+            write_rule_for_code_element(file, selectors_class_keyword_before, tag)
+            write_rule_for_code_element(file, selectors_class_keyword_after, tag)
+            write_rule_for_code_element(file, selectors_attribute_keyword, tag)
+            write_rule_for_code_element(file, selectors_combine_class, tag)
 
-            special_selectors_gitlab = map(
+            special_selectors_gitlab: Iterable[str] = map(
                 lambda x: f'pre[class~="highlight" i] [lang~="{x}" i]', lang
             )
-            file.write(f"    {', '.join(special_selectors_gitlab)} {{\n")
-            file.write(f'        font-feature-settings: "calt" off, "{tag}" on;\n')
-            file.write("    }\n")
+            write_rule_with_whole_selectors(file, special_selectors_gitlab, tag)
+            special_selectors_rustdoc: Iterable[str] = map(
+                lambda x: f'pre[class~="{x}" i]', lang
+            )
+            write_rule_with_whole_selectors(file, special_selectors_rustdoc, tag)
         file.write(r"}")
 
 
